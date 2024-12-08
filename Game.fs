e39f97bb8bfa2bbbaa54cab0a032883cb99cb004
snOlga <olga.safonova@outlook.com>
2024-12-08 12:00:22 +0300

    eating apples

--- Game.fs
+++ Game.fs
@@ -15,12 +15,14 @@ type State =
     {
         Maze: string[][]
         PlayerPosition: Position
+        Score: int
     }
 
 let initGame = 
     {
-        Maze = Maze.mazeMatrix
+        Maze = Maze.MazeMatrix
         PlayerPosition = {X = 0; Y = 0}
+        Score = 0
     }
 
 let printField (gameState:State) =
@@ -44,15 +46,35 @@ let getDirectionInput () =
 
 let moveSomeone position direction =
     match direction with
-    | Direction.Up -> {position with Y = position.Y-1} // inversion of y
-    | Direction.Left -> {position with X = position.X-1}
-    | Direction.Down -> {position with Y = position.Y+1}
-    | Direction.Right -> {position with X = position.X+1}
-    | Direction.None -> position
+    | Direction.Up when position.Y > 0 && 
+        Maze.MazeMatrix[position.Y-1][position.X] <> Maze.WALL -> 
+        { position with Y = position.Y-1 } // inversion of y
+    | Direction.Left when position.X > 0 && 
+        Maze.MazeMatrix[position.Y][position.X-1] <> Maze.WALL -> 
+        { position with X = position.X-1 }
+    | Direction.Down when position.Y < (Maze.MazeMatrix.Length - 1) && 
+        Maze.MazeMatrix[position.Y+1][position.X] <> Maze.WALL -> 
+        { position with Y = position.Y+1 }
+    | Direction.Right when position.Y < (Maze.MazeMatrix.Length - 1) && 
+        Maze.MazeMatrix[position.Y][position.X+1] <> Maze.WALL -> 
+        { position with X = position.X+1 }
+    | _ -> position
+
+let eatApple (gameState:State) =
+    let playerX = gameState.PlayerPosition.X
+    let playerY = gameState.PlayerPosition.Y
+    if gameState.Maze[playerY][playerX] = Maze.APPL then
+        gameState.Maze |> Array.mapi (fun i line -> 
+            if i = playerY then 
+                line |> Array.mapi (fun j chr -> if j = playerX then Maze.EMPT else chr)
+            else
+                line)
+    else
+        gameState.Maze
 
 let rec run (gameState:State) =
     Thread.Sleep(100)
     Console.Clear()
     printField gameState
     let inputDirection = getDirectionInput ()
-    run { gameState with PlayerPosition = (moveSomeone gameState.PlayerPosition inputDirection) }
+    run { gameState with PlayerPosition = (moveSomeone gameState.PlayerPosition inputDirection); Maze = eatApple gameState}
