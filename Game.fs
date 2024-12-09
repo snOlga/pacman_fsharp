35aa318198a888989a04930932e507108e3a7151
snOlga <olga.safonova@outlook.com>
2024-12-09 16:12:16 +0300

    can lose and restart

--- Game.fs
+++ Game.fs
@@ -16,6 +16,8 @@ type State =
         Maze: string[][]
         PlayerPosition: Position
         Score: int
+        Lifes: int
+        IsRunning: bool
         NPC1Position: Position
     }
 
@@ -24,7 +26,9 @@ let initGame =
         Maze = Maze.MazeMatrix
         PlayerPosition = {X = 1; Y = 1; FutureDirection = Direction.None}
         Score = 0
-        NPC1Position = {X = 11; Y = 11; FutureDirection = Direction.None}
+        Lifes = 3
+        IsRunning = true
+        NPC1Position = {X = 11; Y = 9; FutureDirection = Direction.None}
     }
 
 let printField (gameState:State) =
@@ -38,9 +42,16 @@ let printField (gameState:State) =
             | _ -> printf "%s" chr
         printfn ""
 
+let printLifes lifes =
+    for i = 1 to lifes do
+        printf "❤️ "
+    ()
+
 let printGame(gameState:State) =
     printField gameState
-    printfn "%A" gameState.Score
+    printfn "Your score: %A" gameState.Score
+    printfn "Pacman's lifes: "
+    printLifes gameState.Lifes
 
 let getDirectionInput () =
     if not Console.KeyAvailable then
@@ -61,12 +72,16 @@ let moveSomeone position =
     | Direction.Left when position.X > 0 && 
         Maze.MazeMatrix[position.Y][position.X-1] <> Maze.WALL -> 
         { position with X = position.X-1 }
+    | Direction.Left when position.X = 0  -> 
+        { position with X = Maze.MazeMatrix.Length-1 }
     | Direction.Down when position.Y < (Maze.MazeMatrix.Length - 1) && 
         Maze.MazeMatrix[position.Y+1][position.X] <> Maze.WALL -> 
         { position with Y = position.Y+1 }
-    | Direction.Right when position.Y < (Maze.MazeMatrix.Length - 1) && 
+    | Direction.Right when position.X < (Maze.MazeMatrix.Length - 1) && 
         Maze.MazeMatrix[position.Y][position.X+1] <> Maze.WALL -> 
         { position with X = position.X+1 }
+    | Direction.Right when position.X = (Maze.MazeMatrix.Length - 1) -> 
+        { position with X = 0 }
     | _ -> position
 
 let moveNPC position =
@@ -104,13 +119,51 @@ let countScore (gameState:State) =
     else
         gameState.Score
 
+let loseLife (gameState:State) =
+    if gameState.PlayerPosition.X = gameState.NPC1Position.X && gameState.PlayerPosition.Y = gameState.NPC1Position.Y then
+        (gameState.Lifes - 1)
+    else
+        gameState.Lifes
+
+let checkLosing (gameState:State) =
+    gameState.Lifes > 0
+    
+let printLose (gameState:State) =
+    printField { gameState with Maze = ( gameState.Maze |> Array.map (fun line -> line |> Array.map (fun chr -> "❌")))}
+    printfn "You losed!"
+
+let restartGame (gameState:State) =
+    printfn "Want to restart?"
+    printfn "Press Y for yes, press N for quit game"
+    match Console.ReadKey().Key with
+        | ConsoleKey.Y -> initGame
+        | ConsoleKey.N -> gameState
+        | _ -> gameState
+
 let rec run (gameState:State) =
-    Thread.Sleep(200)
-    Console.Clear()
-    printGame gameState
-    let inputDirection = getDirectionInput ()
-    let movedPlayer = moveSomeone { gameState.PlayerPosition with FutureDirection = inputDirection}
-    let movedNPC1 = moveNPC gameState.NPC1Position
-    let mazeNoApple = eatApple gameState
-    let countedScore = countScore gameState
-    run { gameState with PlayerPosition = movedPlayer; Maze = mazeNoApple; Score = countedScore; NPC1Position = movedNPC1}
+    match gameState.IsRunning with
+    | true ->
+        Thread.Sleep(200)
+        Console.Clear()
+        printGame gameState
+        let inputDirection = getDirectionInput ()
+        let movedPlayer = moveSomeone { gameState.PlayerPosition with FutureDirection = inputDirection}
+        let movedNPC1 = moveNPC gameState.NPC1Position
+        let mazeNoApple = eatApple gameState
+        let countedScore = countScore gameState
+        let newLifes = loseLife gameState
+        let isRunning = checkLosing gameState
+        run { Maze = mazeNoApple;
+                PlayerPosition = movedPlayer; 
+                Score = countedScore; 
+                NPC1Position = movedNPC1;
+                Lifes = newLifes;
+                IsRunning = isRunning }
+    | false -> 
+        Console.Clear()
+        printLose gameState
+        let willRestart = restartGame gameState
+        match willRestart.IsRunning with
+        | false -> exit(0)
+        | true -> run willRestart
+        
